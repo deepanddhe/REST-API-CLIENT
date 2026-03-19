0b917a7f0f7078cd7c6aecfe76b5f511e4a6c54d
deepanddhe <msdggold2023@gmail.com>
2026-03-19 14:52:45 +0530

    Update ApiClient.java

--- ApiClient.java
+++ ApiClient.java
@@ -4,25 +4,31 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 public class ApiClient {
-     public static void main(String[] args) {
-     try {
-          URL url = new URI("https://api.agify.io/?name=deepan").toURL();
-           HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-           conn.setRequestMethod("GET");
-           int status = conn.getResponseCode();
-           if (status == 200) {
-                 BufferedReader br = new BufferedReader(
-                 new InputStreamReader(conn.getInputStream())
-                 );
+    public static void main(String[] args) {
+        try {
+            URL url = new URI("https://api.agify.io/?name=deepan").toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            int status = conn.getResponseCode();
+            if (status == 200) {
+                        BufferedReader br = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream())
+                );
                 String line;
                 String result = "";
                 while ((line = br.readLine()) != null) {
                     result = result + line;
                 }
                 br.close();
-                System.out.println("Response from API:");
-                System.out.println(result);
-              } else {
+                String name = result.split("\"name\":\"")[1].split("\"")[0];
+                String age = result.split("\"age\":")[1].split(",")[0];
+                String count = result.split("\"count\":")[1].split("}")[0];
+                System.out.println("----- API RESULT -----");
+                System.out.println("Name   : " + name);
+                System.out.println("Age    : " + age);
+                System.out.println("Count  : " + count);
+
+            } else {
                 System.out.println("Error in API call");
             }
         } catch (Exception e) {
